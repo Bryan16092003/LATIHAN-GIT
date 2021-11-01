ca23a4723e6ca3cebf0e8b7918c9eafae16461c5
Bryan16092003 <Bryanamping50@gmail.com>
2021-10-31 13:40:09 -1200

    commit kelima

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 console.log("Baris pertama");
 console.log("Edit dari github");
 console.log("Test conflig");
+console.log("baris kelima");
 //selasa 
 console.log("Baris kedua");
 console.log("Baris ketiga");
