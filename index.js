639322600a60074fb9147f3b3bef2c8b6a5cda48
Bryan16092003 <Bryanamping50@gmail.com>
2021-10-31 13:26:40 -1200

    commit ketiga

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //Senin
 console.log("Baris pertama");
 console.log("Edit dari github");
+console.log("Test conflict from local")
 //selasa 
 console.log("Baris kedua");
 console.log("Baris ketiga");
